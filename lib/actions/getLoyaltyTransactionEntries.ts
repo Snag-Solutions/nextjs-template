cff5b00c0e7c6ef6d5718f0b94863742df9b7a80
Haseeb <haseebahmad109@gmail.com>
2025-04-11 16:28:31 +0500

    remove comments

--- lib/actions/getLoyaltyTransactionEntries.ts
+++ lib/actions/getLoyaltyTransactionEntries.ts
@@ -33,29 +33,4 @@ export async function getLoyaltyTransactionEntries(
   )
 
   return response.data
-
-  //   // userCompletedLoyaltyRuleId can be a string or an array of strings
-  //   // we need to convert it to use URLSearchParams
-  //   const query = new URLSearchParams()
-  //   if (Array.isArray(userCompletedLoyaltyRuleId)) {
-  //     userCompletedLoyaltyRuleId.forEach((id) => {
-  //       query.append('userCompletedLoyaltyRuleId', id)
-  //     })
-  //   } else if (typeof userCompletedLoyaltyRuleId === 'string') {
-  //     query.append('userCompletedLoyaltyRuleId', userCompletedLoyaltyRuleId)
-  //   }
-
-  //   const path = `/api/loyalty/transaction_entries?${query.toString()}`
-  //   console.log(path)
-  //   return snag.loyalty.transactions.getTransactionEntries(
-  //     {
-  //       organizationId: process.env.NEXT_PUBLIC_ORGANIZATION_ID!,
-  //       websiteId: process.env.NEXT_PUBLIC_WEBSITE_ID!,
-  //       ...params,
-  //     },
-  //     {
-  //       path,
-  //       query,
-  //     }
-  //   )
 }
